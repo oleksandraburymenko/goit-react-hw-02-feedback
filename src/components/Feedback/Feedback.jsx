e47dfa0357c7ad6a5c2842c6abd11f2242d85bfd
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-05-18 18:57:44 +0300

    Add Feedback code

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Section from './Section';
+import ControlsBtn from './ControlsBtn';
+import Stats from './Stat';
+
+
+class Feedback extends React.Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    increment = state => {
+        this.setState(prevState => ({
+            [state]: prevState[state] + 1,
+        }));
+    };
+
+    totalValue = () => {
+        const { good, neutral, bad } = this.state;
+        return good + neutral + bad;
+    };
+
+    positiveFeedbackValue = () => {
+        return this.state.good !== 0
+        ? Math.round((this.state.good / this.totalValue()) * 100)
+        : 0;
+    }
+
+    render() {
+        const { good, bad, neutral } = this.state;
+        const data = Object.keys(this.state);
+        return (
+        <>   
+            <Section title="To leave your feedback, please press the button">
+                <ControlsBtn
+                data={data}
+                increment={this.increment}
+                />
+            </Section>
+            <Section>
+                {this.totalValue() > 0 ? (
+                    <Stats
+                    good={good}
+                    neutral={neutral}
+                    bad={bad}
+                    totalValue={this.totalValue()}
+                    positiveFeedbackValue={this.positiveFeedbackValue()}
+                    />) : (<p>"No feedback yet</p>)
+                }
+            </Section>
+        </> 
+        )
+    }
+}
+
+export default Feedback
